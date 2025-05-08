753ca27e0ecf595725d57b7158f756f8e0dfa20c
jangrarajat <rajatjangra940@gmail.com>
2025-05-08 22:05:26 +0530

    Update logo , Name , ANd chanje UI desine

--- spotify clone/script.js
+++ spotify clone/script.js
@@ -10,30 +10,48 @@ let rangeInput = document.querySelector("#rangeInput")
 let buttonbar = document.querySelector(".fa-backward")
 let playsong = document.querySelector("#playsong")
 let mysongname = document.querySelector("#mysongname")
-const songs = [
-    { name: "Make Some Noise ", path: "/songs/1.mp3", cover: "/images/1.jpg" },
-    { name: "💕Heart Touching Jukebox ❤️", path: "/songs/2.mp3", cover: "/images/2.jpg" },
-    { name: "2 Numbari ", path: "/songs/3.mp3", cover: "/images/3.jpg" },
-    { name: "5 Taara ", path: "/songs/4.mp3", cover: "/images/4.jpg" },
-    { name: "Thunder - Imagine Dragons", path: "/songs/5.mp3", cover: "/images/5.jpg" },
-    { name: "Faded - Alan Walker", path: "/songs/6.mp3", cover: "/images/6.jpg" },
-    { name: "Cheap Thrills - Sia", path: "/songs/7.mp3", cover: "/images/7.jpg" },
-    { name: "Closer - The Chainsmokers", path: "/songs/8.mp3", cover: "/images/8.jpg" },
-    { name: "Let Me Love You - DJ Snake", path: "/songs/9.mp3", cover: "/images/9.jpg" },
-    { name: "Starboy - The Weeknd", path: "/songs/10.mp3", cover: "/images/10.jpg" },
-    { name: "3 Peg - Sharry Mann", path: "/songs/11.mp3", cover: "/images/10.jpg" }
+let songNum = 0;
+let songs = [
+    { songCatagery: "bollywood",name: "Make Some Noise ", path: "/songs/1.mp3", cover: "/images/1.jpg" },
+    { songCatagery: "bollywood",name: "💕Heart Touching Jukebox ❤️", path: "/songs/2.mp3", cover: "/images/2.jpg" },
+    { songCatagery: "haryanvi",name: "2 Numbari ", path: "/songs/3.mp3", cover: "/images/3.jpg" },
+    { songCatagery: "panjabi",name: "5 Taara ", path: "/songs/4.mp3", cover: "/images/4.jpg" },
+    { songCatagery: "panjabi",name: "Thunder - Imagine Dragons", path: "/songs/5.mp3", cover: "/images/5.jpg" },
+    { songCatagery: "panjabi",name: "Faded - Alan Walker", path: "/songs/6.mp3", cover: "/images/6.jpg" },
+    { songCatagery: "boll",name: "Cheap Thrills - Sia", path: "/songs/7.mp3", cover: "/images/7.jpg" },
+    { songCatagery: "panjabi",name: "Closer - The Chainsmokers", path: "/songs/8.mp3", cover: "/images/8.jpg" },
+    { songCatagery: "panjabi",name: "Let Me Love You - DJ Snake", path: "/songs/9.mp3", cover: "/images/9.jpg" },
+    { songCatagery: "panjabi",name: "Starboy - The Weeknd", path: "/songs/10.mp3", cover: "/images/10.jpg" },
+    { songCatagery: "panjabi",name: "3 Peg - Sharry Mann", path: "/songs/11.mp3", cover: "/images/10.jpg" }
 ];
+
+
+let logosection = document.querySelector("#logo-section")
 
 //  music cantrol bAR 
 
 
-let songNum = 0;
+ 
 let audiobar = document.createElement("audio")
 audiobar.setAttribute("src", songs[`${songNum}`].path)
 mysongname.innerText = songs[`${songNum}`].name
 audiobar.setAttribute("controls", "controls")
 let icons = document.querySelector(".containor")
 buttonbar.after(audiobar)
+
+logosection.addEventListener("click", ()=>{
+    filterfun()
+})
+
+function filterfun(){
+console.log(songs)
+songs = songs.filter((bollywood)=> bollywood.songCatagery  === "panjabi" ) 
+console.log(songs)
+audiobar.setAttribute("src", songs[`${songNum}`].path)
+mysongname.innerText = songs[`${songNum}`].name
+
+}
+
 
 
 
@@ -42,6 +60,10 @@ buttonbar.after(audiobar)
 // next song play button 
 
 nextSongBtn.addEventListener("click", () => {
+    next()
+})
+
+function next(){
     if (songs.length >= songNum) {
         songNum += 1
     } else {
@@ -52,8 +74,7 @@ nextSongBtn.addEventListener("click", () => {
     audiobar.play()
     console.log(songs[`${songNum}`].name)
     mysongname.innerText = songs[`${songNum}`].name
-})
-
+}
 
 // previous song play button 
 
@@ -106,8 +127,9 @@ window.addEventListener("keydown", (e) => {
         audiobar.pause()
     }
 
-
-
-
-
 })
+
+
+audiobar.addEventListener("ended", next);
+
+
